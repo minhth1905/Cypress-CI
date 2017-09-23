7065ad2b1c3d35cdad57e2947849c5d68db1b18e
Tran Huu Minh <minhth1905@gmail.com>
2017-09-23 16:57:16 +0700

    add travis

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -3,7 +3,6 @@ describe('THEMMOI_BN', function() {
   it("login", function() {
     cy.visit("http://52.187.8.102/signin")
     cy.get('input[name=email]').type('doctor_10@gmail.com')
-    cy.get('input[name=password]').type('Methadone@2017')
-    cy.get('button[type=submit]').click()
+    cy.get('input[name=password]').type('Methadone@2017{enter}')
   })
 })
